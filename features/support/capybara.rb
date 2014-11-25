5a9a503ac000ddd4e115cbb2999145894b127060
Aaron Kromer <aaron.kromer@gmail.com>
2014-11-25 12:10:35 -0500

    Handle feature specs without Capybara.
    When Capybara has not been loaded the RSpec suite fails with syntax
    errors on `feature` and `scenario`. While Capybara is an external gem
    dependency, rspec-rails is so tightly coupled with it, necessitating
    some basic support for the syntax.
    
    This adds coverage for both the external documentation cukes and the
    Rails app smoke specs. To prevent RSpec from raising confusing syntax
    errors when Capybara is not available we shim on top of the basic
    syntax: `feature` and `scenario`. Without both of these we receive
    syntax failures with basic feature specs.
    
    Since RSpec does not support the reset of the Capybara methods / DSL the
    specs are skipped by default. A helpful message about using Capybara is
    displayed when skipped.
    
    Updates the cuke to match changes from #1231. This uses the `RSpec`
    namespace for calling `feature`. It also adds the `:type => :feature`
    metadata. rspec-rails needs to hook into feature example groups, this
    change ensures that happens by setting the appropriate `:type`.
    
    While Capybara does include the `:type` metadata automatically it may
    not always. Most of the Capybara related hooks actually rely on the
    metadata: `:capybara_feature => true`.

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,4 +1,5 @@
 Around "@capybara" do |scenario, block|
+  require 'capybara' rescue nil
   # We are caught in a weird situation here. rspec-rails supports 1.8.7 and
   # above, but capybara beyond a certain version only supports 1.9.3 and above.
   # On 1.8.7 and 1.9.2, we run most of the rspec-rails test suite but leave out
